03b07295e2969fde05f7d7001940c94ac8f70d47
kitthornton <kit.thornton16@gmail.com>
2021-10-06 11:24:52 +0100

    allow for bouldering routing also

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,32 @@ router.get('/climbingGrades', async function(req, res) {
   }
 });
 
+// GET all the climbing grades with descriptions
+router.get('/grades/routeClimbing', async function(req, res) {
+  try {
+    const q = 'SELECT * FROM dbo.vwgradeswithdescription';
+    const todos = await pool.query(q)
+    res.json(todos);
+    console.log("Retrieved all items from dbo.vwgradeswithdescription")
+
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// GET all the climbing grades with descriptions
+router.get('/grades/bouldering', async function(req, res) {
+  try {
+    const q = 'SELECT * FROM dbo.vwgradeswithdescription';
+    const todos = await pool.query(q)
+    res.json(todos);
+    console.log("Retrieved all items from dbo.vwgradeswithdescription")
+
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 // GET all the expertise levels
 router.get('/expertiseLevels', async function(req, res) {
   try {
